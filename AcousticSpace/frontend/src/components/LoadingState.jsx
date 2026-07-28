e4b93ae2c46088b99405f4813fcc3b2ab7397016
Shivangi Trivedi <shivangitrivedo0531@gmail.com>
2026-07-28 14:15:22 +0530

    Add navigation button to loading state

--- AcousticSpace/frontend/src/components/LoadingState.jsx
+++ AcousticSpace/frontend/src/components/LoadingState.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./LoadingState.css";
+import { useNavigate } from "react-router-dom";
 
 const LoadingState = () => {
-  return (
+  const navigate = useNavigate();
+ return (
     <div className="loading-container">
       <div className="spinner"></div>
 
@@ -11,6 +13,13 @@ const LoadingState = () => {
       <p>Please wait while AcousticSpace analyzes the uploaded audio.</p>
 
       <span>This may take a few seconds.</span>
+
+      <button
+        className="loading-btn"
+        onClick={() => navigate("/results")}
+      >
+        View Demo Result
+      </button>
     </div>
   );
 };
